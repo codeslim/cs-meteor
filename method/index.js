71575838f19ce1cd7963a0c5f9a63cae2939ca2d
Michel Antoine <michel1.antoine@epitech.eu>
2015-12-08 19:44:27 -0800

    Add meteor method support

---
+++ method/index.js
@@ -0,0 +1,60 @@
+'use strict';
+
+let generators = require('yeoman-generator');
+let _ = require('lodash');
+let fs = require('fs');
+let path = require('path');
+
+module.exports = generators.Base.extend({
+  constructor: function() {
+    generators.Base.apply(this, arguments);
+  },
+
+  _optionOrPrompt: require('yeoman-option-or-prompt'),
+
+  _getResources() {
+    if (typeof this.resources === 'undefined') {
+      this.resources = fs.readdirSync(
+        this.destinationPath('lib/collections')
+      ).map(elem => path.basename(elem, '.js'));
+    }
+    return this.resources;
+  },
+
+  prompting: function() {
+    let done = this.async();
+    let router = this.config.get('router');
+
+    this._optionOrPrompt([
+      {
+        message: 'What kind of method?',
+        type: 'list',
+        default: 'insert',
+        choices: ['insert', 'update', 'read', 'delete'],
+        name: 'kind'
+      },
+      {
+        message: 'For which resource?',
+        type: 'list',
+        choices: this._getResources(),
+        name: 'resource'
+      }],
+    answers => {
+      this.kind = answers.kind;
+      this.resource = answers.resource;
+      done();
+    });
+  },
+
+  writing: function() {
+    let collection = _.capitalize(this.resource);
+
+    this.fs.copyTpl(
+      this.templatePath(`${this.kind}.ejs`),
+      this.destinationPath(`lib/methods/${this.kind}/${this.resource}.js`),
+      {
+        collection
+      }
+    )
+  }
+});
