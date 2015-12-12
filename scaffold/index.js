1e6695c3dfce5e935625d15f7554bb82557b8bdb
Michel Antoine <michel1.antoine@epitech.eu>
2015-12-11 21:57:00 -0800

    Add scaffold

---
+++ scaffold/index.js
@@ -0,0 +1,41 @@
+'use strict';
+
+let generators = require('yeoman-generator');
+let _ = require('inflection');
+
+module.exports = generators.Base.extend({
+  constructor: function() {
+    generators.Base.apply(this, arguments);
+    this.argument('resourceName', { type: String, required: true });
+  },
+
+  writing: function() {
+    let name = _.titleize(_.pluralize(this.resourceName.toLowerCase()));
+
+    this.composeWith('joker:collection', {
+      options: {
+        addToCollection: true
+      },
+      args: [name]
+    });
+    ['insert', 'update', 'read', 'delete'].forEach(kind => {
+        this.composeWith('joker:method', {
+          options: {
+            kind,
+            resource: name
+          }
+        })
+      }
+    );
+    ['new', 'list', 'show', 'edit'].forEach(kind => {
+        this.composeWith('joker:view', {
+          options: {
+            kind,
+            resource: name,
+            route: true
+          }
+        })
+      }
+    );
+  }
+});
